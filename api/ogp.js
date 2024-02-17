f7b1bfddbf874c159f9a5e5807ea783b9cfe2ec9
nimono3 <77790085+nimono3@users.noreply.github.com>
2024-02-17 20:01:06 +0900

    Update ogp.js

--- api/ogp.js
+++ api/ogp.js
@@ -1,3 +1,17 @@
 export default function main(req, res) {
-  res.send(req.query);
+  const { url: try_url } = req.query;
+  if (!(try_url + "").match(/^https:\/\//)) res.send(JSON.stringify({}));
+  fetch(try_url)
+    .then(text => {
+      const el = new DOMParser().parseFromString(text, "text/html");
+      const ogp = {};
+      [...el.head.children].map(child => {
+          const property = child.getAttribute("property");
+          if (!property.match(/^og:/)) return;
+          ogp[property.replace(/^og:/, "")] = child.getAttribute("content");
+      });
+      ogp["image"] = ogp["image"].match(/:\/\//) ? ogp["image"] : ogp["url"] + ogp["image"];
+      return ogp;
+    })
+    .then(ogp => res.send(ogp))
 }
